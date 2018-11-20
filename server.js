06ecf4ce5965e93413d43ac4a5a17ff29a8a5435
LeanKhan <eslean20@gmail.com>
2018-11-20 23:41:02 +0100

    Start building server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const socket = require("socket.io");
+const server = require("http").Server(app);
+
+require("dotenv").config();
+app.use(cors());
+
+const port = process.env.DEV_PORT;
+const host = process.env.DEV_HOST;
+
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+let io = socket(server);
+
+io.on("connection", function(socket) {
+  console.log("New Socket Connected at " + socket.id);
+  socket.on("message", function(message) {
+    io.emit("message", message);
+    console.log(message);
+  });
+});
+
+server.listen(port, host, function() {
+  console.log(`App listening on ${host}:${port}`);
+});
