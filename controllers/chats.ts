14b9e79c0d03a7f20056dd666db7b650ae676f70
LeanKhan <eslean20@gmail.com>
2019-08-11 23:36:37 +0100

    feat: convert Post model to ts & chats router

---
+++ controllers/chats.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import { Socket } from 'socket.io';
+const chatsRouter = express.Router();
+
+const listener = function(socket: Socket) {
+  // console.log("Connection from chat?"+socket.id);
+  socket.on('chat', function() {
+    console.log('Chat!');
+  });
+};
+
+chatsRouter.route('/').get((req, res) => {
+  res.send('Inside /chats');
+});
+
+chatsRouter.route('/:id').get((req, res) => {
+  const id = req.params.id;
+  res.send(`Inside /chats/${id}`);
+});
+
+export default {
+  listener,
+  router: chatsRouter,
+};
